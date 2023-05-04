1701241e45ca9f898669e71885fb24fa85a7bf31
omkar00015 <100870962+omkar00015@users.noreply.github.com>
2023-05-04 16:42:21 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+function myFunc() {
+    window.open("https://www.youtube.com/watch?v=Se26Op9sEC8");
+}
+
+function func () {
+    window.open("https://www.youtube.com/@Netflix");
+}
+
+function func1 () {
+    window.open("https://www.youtube.com/watch?v=lV6sJlBbhPs");
+}
+function func2 () {
+    window.open("https://www.youtube.com/watch?v=mO0OuR26IZM&t=2s");
+}
+function func3 () {
+    window.open("https://www.youtube.com/watch?v=E1KDuaPrUaY");
+}
+function func4() {
+    window.open("https://www.youtube.com/watch?v=KZKvOzlYit4");
+}
+function func5() {
+    window.open("https://www.youtube.com/watch?v=Way9Dexny3w");
+}
+function func6() {
+    window.open("https://www.youtube.com/watch?v=itnqEauWQZM&t=19s");
+}
+function func7() {
+    window.open("https://www.youtube.com/@WarnerBrosPictures");
+}
+function func8() {
+    window.open("https://www.youtube.com/@paramountpictures");
+}
+function func9() {
+    window.open("https://www.youtube.com/watch?v=19ikl8vy4zs");
+}
+function func10() {
+    window.open("https://www.youtube.com/@sonypictures");
+}
